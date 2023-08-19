5238c49ec4900c9fa3108540190109255ea7576c
BS-Eg <120326341+BS-Eg@users.noreply.github.com>
2023-08-19 13:57:02 -0400

    Update odoo-venv.sh

--- odoo-venv.sh
+++ odoo-venv.sh
@@ -66,41 +66,43 @@
     exit
 #  " 9. Create the odoo.conf file using the nano text editor: "
 
-    sudo nano /odoo/151/odoo.conf
+    sudo touch /odoo/151/odoo.conf
 
-    [options]
-    ; Database operations password:
-    admin_passwd = PASSWORD
-    db_host = False
-    db_port = False
-    db_user = odoo
-    db_password = False
-    addons_path = /opt/odoo/odoo/addons,/opt/odoo/odoo-custom-addons
+    cat <<EOF >odoo.conf
+
+        [options]
+        ; Database operations password:
+        admin_passwd = PASSWORD
+        db_host = False
+        db_port = False
+        db_user = odoo
+        db_password = False
+        addons_path = /opt/odoo/odoo/addons,/opt/odoo/odoo-custom-addons
+
+    EOF    
 
 #  " 10. Create the odoo.service file with: "
 
-    sudo nano /etc/systemd/system/odoo.service
-    cat <<EOF >filename
-    first line
-    second line
-    third line
+    touch /lib/systemd/system/odoo.service
+    cat <<EOF >odoo.service
+        [Unit]
+        Description=Odoo
+        Requires=postgresql.service
+        After=network.target postgresql.service
+        [Service]
+        Type=simple
+        SyslogIdentifier=odoo
+        PermissionsStartOnly=true
+        User=odoo
+        Group=odoo
+        ExecStart=/opt/odoo/odoo-venv/bin/python3 /opt/odoo/odoo/odoo-bin -c /etc/odoo.conf
+        StandardOutput=journal+console
+        [Install]
+        WantedBy=multi-user.target
     EOF
     #    Paste the following contents into the file:
 
-    [Unit]
-    Description=Odoo
-    Requires=postgresql.service
-    After=network.target postgresql.service
-    [Service]
-    Type=simple
-    SyslogIdentifier=odoo
-    PermissionsStartOnly=true
-    User=odoo
-    Group=odoo
-    ExecStart=/opt/odoo/odoo-venv/bin/python3 /opt/odoo/odoo/odoo-bin -c /etc/odoo.conf
-    StandardOutput=journal+console
-    [Install]
-    WantedBy=multi-user.target
+ 
 
 
 #  "  11. Update the service list: "
